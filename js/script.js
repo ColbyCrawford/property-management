59a4e869e96042b0db7e9494ac345bc7634aef84
Colby <colbyjcrawford@gmail.com>
2023-03-01 14:37:14 -0800

    property management project

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+const bgTransition = document.querySelector('.bg-transition');
+const themeToggle = document.querySelector('.theme-toggle');
+
+themeToggle.addEventListener('change', () => {
+    const mainNav = document.querySelector('.main-nav');
+    if(themeToggle.checked) {
+        
+
+        body.classList.remove('light');
+        body.classList.add('dark');
+
+        if(mainNav == null) {
+            logo.src = '/images/logo--light.svg';
+        } else {
+            logo.src = '/images/logo--dark.svg';
+        }
+
+        bgTransition.src = '/images/bg-transition--dark.svg';
+
+        console.log("dark mode enabled!");
+    } else {
+        body.classList.remove('dark');
+        body.classList.add('light');
+        if(mainNav == null) {
+            logo.src = '/images/logo--dark.svg';
+        } else {
+            logo.src = '/images/logo--light.svg';
+        }
+        bgTransition.src = '/images/bg-transition--light.svg';
+
+        console.log("light mode enabled!");
+    }
+});
