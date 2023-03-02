a1ca7be745ecbe464fee970cc3efea296d977c58
Colby <colbyjcrawford@gmail.com>
2023-03-01 20:15:35 -0800

    file path fixing v11

--- src/js/script.js
+++ src/js/script.js
@@ -12,23 +12,23 @@ themeToggle.addEventListener('change', () => {
         body.classList.add('dark');
 
         if(mainNav == null) {
-            logo.src = '../images/logo--light.svg';
+            logo.src = '/images/logo--light.svg';
         } else {
-            logo.src = '../images/logo--dark.svg';
+            logo.src = '/images/logo--dark.svg';
         }
 
-        bgTransition.src = '../images/bg-transition--dark.svg';
+        bgTransition.src = '/images/bg-transition--dark.svg';
 
         console.log("dark mode enabled!");
     } else {
         body.classList.remove('dark');
         body.classList.add('light');
         if(mainNav == null) {
-            logo.src = '../images/logo--dark.svg';
+            logo.src = '/images/logo--dark.svg';
         } else {
-            logo.src = '../images/logo--light.svg';
+            logo.src = '/images/logo--light.svg';
         }
-        bgTransition.src = '../images/bg-transition--light.svg';
+        bgTransition.src = '/images/bg-transition--light.svg';
 
         console.log("light mode enabled!");
     }
