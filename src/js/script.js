78cac8189966e1487205da57a32cd376432e612a
Colby <colbyjcrawford@gmail.com>
2023-03-03 16:27:43 -0800

    fixed mobile & tablet navigvation

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,9 @@ const body = document.querySelector('body');
 const logo = document.querySelector('.logo');
 const bgTransition = document.querySelector('.bg-transition');
 const themeToggle = document.querySelector('.theme-toggle');
+const menuToggle = document.querySelector('.menu-toggle');
+const mainNavLinks = document.querySelector('.main-nav__links');
+const overlay = document.querySelector('#overlay');
 
 themeToggle.addEventListener('change', () => {
     const mainNav = document.querySelector('.main-nav');
@@ -32,4 +35,33 @@ themeToggle.addEventListener('change', () => {
 
         console.log("light mode enabled!");
     }
+});
+
+menuToggle.addEventListener('click', () => { 
+    const icon = document.querySelector('.menu-toggle__icon');
+    const mainNav = document.querySelector('.main-nav');
+    if (mainNavLinks.hasAttribute('data-visible')) {
+        icon.classList.add('fa-bars');
+        icon.classList.remove('fa-xmark');     
+        if (mainNav == null) {
+            if (body.classList.contains('light')) {
+                icon.classList.remove('text-primary-400');
+            } else {
+                icon.classList.remove('text-neutral-200');
+            }
+        } 
+    } else {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-xmark');
+        if (mainNav == null) {
+            if (body.classList.contains('light')) {
+                icon.classList.add('text-primary-400');
+            } else {
+                icon.classList.add('text-neutral-200');
+            }
+            
+        }
+    }
+    mainNavLinks.toggleAttribute('data-visible');
+    overlay.toggleAttribute('data-visible');
 });
